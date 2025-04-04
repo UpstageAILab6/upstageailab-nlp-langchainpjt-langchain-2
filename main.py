c5913b14a4ff4d6da7b932f7d36bc8917602ae47
jeong junseong <130049734+JeongJunSeong@users.noreply.github.com>
2025-04-04 21:38:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import chat_history
 
 if __name__ == "__main__":
-    # 실행할 질문
-    question = "제 이름은 정준성입니다."
+    while True:
+        # 실행할 질문
+        question = input("입력: ")
 
-    # RAG 실행
-    response = chat_history.chain_with_history.invoke(
-        {"question": question},
-        config={"configurable": {"session_id": "rag123"}},
-    )
+        # RAG 실행
+        response = chat_history.chain_with_history.invoke(
+            {"question": question},
+            config={"configurable": {"session_id": "rag123"}},
+        )
 
-    print(response)
+        print(response)
