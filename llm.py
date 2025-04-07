e8a78c326b443489f91eb398d4cf85da05644fda
shayoyou <45893165+shayoyou@users.noreply.github.com>
2025-04-07 12:41:27 +0900

    Update llm.py
    ## 프롬프트 템플릿 변경
    - 한국어로 작성
    - 상세한 지시
    
    ## prompt에 전달되는 내용 메타정보 제거
    - retriever 로 가져온 내용 page_content 만 사용
    - 이전 대화내용 포멧팅 [(대화), (대화), (대화) ...] 형식

--- llm.py
+++ llm.py
@@ -1,5 +1,6 @@
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
 from langchain_upstage import ChatUpstage
 from operator import itemgetter
 import logging
@@ -17,22 +18,28 @@ class LLMChain:
 
     def _create_prompt(self) -> PromptTemplate:
         logger.info("Creating prompt template")
-        return PromptTemplate.from_template(
-            """You are an assistant for question-answering tasks.
-Use the following pieces of retrieved context to answer the question.
-If you don't know the answer, just say that you don't know.
-Answer in Korean.
+        return PromptTemplate.from_template("""
+당신은 고도로 지능적인 QA 엔진입니다. 제공된 문서 참조 내용과 이전 대화 내용을 바탕으로 사용자의 질문에 답변하세요. 답변은 다음 지침을 따르세요:
+1. 문서 참조 내용과 이전 대화 내용을 우선적으로 활용해 정확한 답변을 제공.
+2. 문서나 대화에 정보가 부족하면, 일반 지식을 사용해 보완하되 출처가 다름을 명시.
+3. 간결하고 명확하게 답변하며, 필요 시 간단한 설명 추가.
 
-#Previous Chat History:
-{chat_history}
-
-#Question:
-{question}
-#Context:
+문서 참조 내용:
 {context}
 
-#Answer:"""
+이전 대화 내용:
+{chat_history}
+
+질문:
+{question}
+"""
         )
+    
+    def _get_formatted_history(self, messages):
+        result = []
+        for idx in range(0, len(messages), 2):
+            result.append((f"USER: {messages[idx].content}", f"AI: {messages[idx+1].content}"))
+        return result
 
     def _create_chain(self):
         try:
@@ -41,9 +48,9 @@ Answer in Korean.
             prompt = self._create_prompt()
             return (
                 {
-                    "context": itemgetter("question") | self.retriever,
+                    "context": itemgetter("question") | self.retriever | (lambda x: "\n".join(doc.page_content for doc in x)),
+                    "chat_history": itemgetter("chat_history") | RunnableLambda(self._get_formatted_history),
                     "question": itemgetter("question"),
-                    "chat_history": itemgetter("chat_history"),
                 }
                 | prompt
                 | llm
